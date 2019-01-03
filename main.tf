8a0f92b6dac037ef3f6e1b75949c8191779e4aea
valavan007 <36833834+valavan007@users.noreply.github.com>
2019-01-03 10:17:08 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "google" {
 
 resource "google_compute_instance" "test" {
   count        = 1 // Adjust as desired
-  name         = "test${count.index + 1}" // yields "test1", "test2", etc. It's also the machine's name and hostname
+  name         = "Pipeline${count.index + 1}" // yields "test1", "test2", etc. It's also the machine's name and hostname
   machine_type = "f1-micro" // smallest (CPU &amp; RAM) available instance
   zone         = "us-central1-b" // yields "europe-west1-d" as setup previously. Places your VM in Europe
  
